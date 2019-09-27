f0caee41875012f4b0bd68f1066cae51db411ef0
YoungJun <dudwns9073@nate.com>
2019-09-28 01:07:28 +0900

    FileUpload 컨트롤럴, DTO 생성

--- ts/src/main/java/com/duojj/dto/FileImageDTO.java
+++ ts/src/main/java/com/duojj/dto/FileImageDTO.java
@@ -5,6 +5,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileImageDTO {
 	private MultipartFile file;
 	private String class_id;
+	private String file_name;
+	public String getFile_name() {
+		return file_name;
+	}
+	public void setFile_name(String file_name) {
+		this.file_name = file_name;
+	}
 	public MultipartFile getFile() {
 		return file;
 	}
