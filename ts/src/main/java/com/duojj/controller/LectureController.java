d45e9902b44f72b87d607f53d78e6d7fa3464857
YoungJun <dudwns9073@nate.com>
2019-10-03 01:44:58 +0900

    TestContoller -> LectureController [UPDATE] 로그인 관련 기능 Interceptor로 수정

--- ts/src/main/java/com/duojj/controller/LectureController.java
+++ ts/src/main/java/com/duojj/controller/LectureController.java
@@ -1,16 +1,20 @@
 package com.duojj.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,7 +22,6 @@ import com.duojj.dto.CategoriesDTO;
 import com.duojj.service.LectureService;
 import com.duojj.service.UserService;
 import com.duojj.vo.LectureVO;
-import com.duojj.vo.UserVO;
 import com.google.gson.Gson;
 
 @Controller
@@ -29,43 +32,50 @@ public class LectureController {
 	@Inject
 	private UserService userService;
 	
-	@RequestMapping(value="/form", method=RequestMethod.GET)
-	public ModelAndView getLectureRegister()throws Exception{	
-	    // 데이터와 뷰를 동시에 설정이 가능
-	    ModelAndView mv = new ModelAndView();
-	    mv.setViewName("/home");
-	    return mv;
+	@InitBinder
+	public void InitBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
 	}
 	
-	@RequestMapping(value="/regist", method=RequestMethod.POST)
-	public ModelAndView postLectureRegister(LectureVO vo)throws Exception{
-		ModelAndView mv = new ModelAndView();
-		lectureService.postLectureRegister(vo);
-		mv.setViewName("/"+vo.getClass_id()); // 뷰의 이름 -> 강의 상세 페이지로 들어가야함
-		return mv;
+	@RequestMapping(value = "/regist", method = RequestMethod.POST, headers = ("content-type=*"))
+	@ResponseBody
+	public ResponseEntity<Map<String, String>> postLectureRegister(LectureVO vo) throws Exception {
+		try {
+			lectureService.postLectureRegister(vo);
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("message", "강의 개설 성공");
+			return new ResponseEntity<Map<String, String>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+			System.out.println(e.getMessage());
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("message", "강의 개설 실패");
+			return new ResponseEntity<Map<String, String>>(map, HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@SuppressWarnings("rawtypes")
-	@RequestMapping(value = "/insert", method = RequestMethod.GET)
-	public ModelAndView Classinsert(HttpServletRequest request,HttpServletResponse response, RedirectAttributes rttr) throws Exception {
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public ModelAndView Classinsert(LectureVO vo, RedirectAttributes rttr) throws Exception {
 		ModelAndView mv = new ModelAndView();
-		CategoriesDTO categories = new CategoriesDTO();
-		Map<String, Map> map = categories.getCategories();
-		Gson gson = new Gson();
-		String json = gson.toJson(map);
-		mv.addObject("categories_dict", json);
-		
-		HttpSession session = request.getSession();
-		UserVO userVO = (UserVO)session.getAttribute("login");
-		
-		if(userVO != null) {
+		try {
+			lectureService.postInitLectureRegister(vo);
+			CategoriesDTO categories = new CategoriesDTO();
+			Map<String, Map> map = categories.getCategories();
+			Gson gson = new Gson();
+			String json = gson.toJson(map);
+			mv.addObject("categories_dict", json);
+			mv.addObject("class_id", vo.getClass_id());
 			mv.setViewName("/classinsert");
-		} else {
-			System.out.println("로그인이 안되어있습니다.");
-			rttr.addFlashAttribute("msg", "로그인이 필요합니다.");
-			mv.setViewName("redirect:/user/login");
+			return mv;
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+			System.out.println(e.getMessage());
+			rttr.addFlashAttribute("msg", "다시 시도해 주세요");
+			mv.setViewName("redirect:/main");
+			return mv;
 		}
-		return mv;
 	}
 	
 	@RequestMapping(value="/{class_id}", method=RequestMethod.GET)
