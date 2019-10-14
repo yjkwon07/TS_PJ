54dd3aabfd03464a561fca4c5198339d0aab4886
YoungJun <dudwns9073@nate.com>
2019-10-14 13:56:38 +0900

    ES6로 변환 [UPDATE] - 지역, 전역, 상수 변수의 명확함을 보여준다. -  allow function을 사용하여 'this' binding을 줄여주었다.

--- ts/src/main/webapp/resources/scripts/maps/custommarker.js
+++ ts/src/main/webapp/resources/scripts/maps/custommarker.js
@@ -1,7 +1,7 @@
 // Custom Map Marker
 // https://developers.google.com/maps/documentation/javascript/reference/overlay-view
 // ----------------------------------------------- //
-function CustomMarker(latlng, map, args, markerIco) {
+function CustomMarker(map, latlng, args, markerIco) {
     this.latlng = latlng;
     this.args = args;
     this.markerIco = markerIco;
@@ -10,9 +10,7 @@ function CustomMarker(latlng, map, args, markerIco) {
 }
 CustomMarker.prototype = new google.maps.OverlayView();
 CustomMarker.prototype.draw = function () {
-    // for bind this
-    var self = this;
-    var marker = this.div;
+    let marker = this.div;
     if (!marker) {
         marker = this.div = document.createElement('div');
         marker.className = 'map-marker-container';
@@ -25,20 +23,21 @@ CustomMarker.prototype.draw = function () {
                 </div>
             </div>`;
         // Clicked marker highlight
-        google.maps.event.addDomListener(marker, "click", function (event) {
+        google.maps.event.addDomListener(marker, "click", (_event) => {
             document.querySelectorAll('.map-marker-container').forEach(function (allMarker) {
                 allMarker.classList.remove('clicked', 'infoBox-opened');
             });
-            google.maps.event.trigger(self, "click");
+            google.maps.event.trigger(this, "click");
             marker.classList.add('clicked', 'infoBox-opened');
         });
+
         if (typeof (this.args.marker_id) !== 'undefined') {
             marker.dataset.marker_id = this.args.marker_id;
         }
-        var panes = this.getPanes();
+        const panes = this.getPanes();
         panes.overlayImage.appendChild(marker);
     }
-    var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+    const point = this.getProjection().fromLatLngToDivPixel(this.latlng);
     if (point) {
         marker.style.left = (point.x) + 'px';
         marker.style.top = (point.y) + 'px';
