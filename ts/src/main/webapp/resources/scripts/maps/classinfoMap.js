6ac05f24862d3403fccecb5ba6002b812c3a0713
YoungJun <dudwns9073@nate.com>
2019-10-14 13:59:48 +0900

    classinfoMap [CREATE] - 상세보기창인 classinfo 페이지에 map작업이 이루어지는 곳이다. - setNavigation() 현재 위치에서 개설된 강의까지 갈 수 있는 길찾기 방법을 알려준다.

---
+++ ts/src/main/webapp/resources/scripts/maps/classinfoMap.js
@@ -0,0 +1,68 @@
+// lecture classinfo Script
+//------------------------------------------------
+const elementMap = document.querySelector("#singleListingMap");
+const SINGLE_STYLE = [{ "featureType": "poi", "elementType": "labels.text.fill", "stylers": [{ "color": "#747474" }, { "lightness": "23" }] }, { "featureType": "poi.attraction", "elementType": "geometry.fill", "stylers": [{ "color": "#f38eb0" }] }, { "featureType": "poi.government", "elementType": "geometry.fill", "stylers": [{ "color": "#ced7db" }] }, { "featureType": "poi.medical", "elementType": "geometry.fill", "stylers": [{ "color": "#ffa5a8" }] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [{ "color": "#c7e5c8" }] }, { "featureType": "poi.place_of_worship", "elementType": "geometry.fill", "stylers": [{ "color": "#d6cbc7" }] }, { "featureType": "poi.school", "elementType": "geometry.fill", "stylers": [{ "color": "#c4c9e8" }] }, { "featureType": "poi.sports_complex", "elementType": "geometry.fill", "stylers": [{ "color": "#b1eaf1" }] }, { "featureType": "road", "elementType": "geometry", "stylers": [{ "lightness": "100" }] }, { "featureType": "road", "elementType": "labels", "stylers": [{ "visibility": "off" }, { "lightness": "100" }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffd4a5" }] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [{ "color": "#ffe9d2" }] }, { "featureType": "road.local", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.local", "elementType": "geometry.fill", "stylers": [{ "weight": "3.00" }] }, { "featureType": "road.local", "elementType": "geometry.stroke", "stylers": [{ "weight": "0.30" }] }, { "featureType": "road.local", "elementType": "labels.text", "stylers": [{ "visibility": "on" }] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [{ "color": "#747474" }, { "lightness": "36" }] }, { "featureType": "road.local", "elementType": "labels.text.stroke", "stylers": [{ "color": "#e9e5dc" }, { "lightness": "30" }] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [{ "visibility": "on" }, { "lightness": "100" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#d2e7f7" }] }];
+
+// Marker
+const CURRENT_MAP_ICO = "<i class='im im-icon-Geek-2'></i>";
+const LECTURE_MAP_ICO = "<i class='im im-icon-Street-View'></i>";
+
+// GPS
+const LECTURE_LAT = parseFloat(elementMap.dataset.latitude);
+const LECTURE_LNG = parseFloat(elementMap.dataset.longitude);
+
+// we find out from Current Location to lecture Location
+const currentGeoLocation = document.querySelector('#geoLocation');
+
+// Steet View Button
+const streetelement = document.querySelector("#streetView");
+
+// object
+let googleMap;
+
+// default Google config
+const tools = {
+    position: {
+        lat: LECTURE_LAT,
+        lng: LECTURE_LNG,
+    },
+    elementMap,
+    scrollwheel: false,
+    currentGeoLocation,
+    MAP_STYLE: SINGLE_STYLE,
+}
+
+// Geo Location Current & Create select Button
+async function setNavigation() {
+    await googleMap.removePinMarker();
+    await googleMap.geolocate();
+    await googleMap.addPinMarker(CURRENT_MAP_ICO, 1, parseFloat(googleMap.position.lat), parseFloat(googleMap.position.lng));
+    await googleMap.addPinMarker(LECTURE_MAP_ICO, 2, LECTURE_LAT, LECTURE_LNG);
+
+    const singleListingMapContainer = document.querySelector("#singleListingMap-container");
+    const checkSelect = singleListingMapContainer.querySelector(".js_route_mode_way");
+    if (!checkSelect || typeof (checkSelect) == 'undefined' && checkSelect == null) {
+        const selector = document.createElement("select");
+        selector.classList.add("js_route_mode_way")
+        selector.innerHTML = `
+            <option value="DRIVING">Driving</option>
+            <option value="WALKING">Walking</option>
+            <option value="BICYCLING">Bicycling</option>
+            <option value="TRANSIT">Transit</option>
+        `;
+        singleListingMapContainer.appendChild(selector);
+        // Route find listener  
+        googleMap.routeFind(singleListingMapContainer, LECTURE_LAT, LECTURE_LNG);
+    }
+}
+
+function singleListingMap() {
+    googleMap = new GoogleMap(tools);
+    googleMap.setMapZoom(15);
+    googleMap.setStreetView(streetelement);
+    googleMap.addPinMarker(LECTURE_MAP_ICO, 1, parseFloat(googleMap.position.lat), parseFloat(googleMap.position.lng));
+    currentGeoLocation.addEventListener("click", () => {
+        setNavigation();
+    });
+}
+singleListingMap();
