ecd1d0d21a8db09f11f36e50ba46a2c0d54acf3a
YoungJun <dudwns9073@nate.com>
2019-10-14 13:55:15 +0900

    mainMap [CREATE] - origin mainMap.js는 backup 폴더로 옮겼다. - 현재 mainMap에서는 mainPage에서 지도를 보여주는 기능을 담았다.
    init()
    - setNavigation() 에서 길찾기 버튼을 클릭시 현재 내위치에서 해당 검색한 강의에까지 도착하는 방법을 알려준다.
      - DRIVING, WARLGING, BICYCLING, TRASIT 기능이 있다.
    - setStreetView() 현재 주변의 거리 뷰를 보여준다.
    - getBounds() 강의 검색 시작 버튼을 클릭 시 getBounds addListener를 실행한다.
      - zoom_changed, dragend
      - 줌 또는 드래그, 화면이동 등 지도 정보 변경시에 화면내에 마커만 표시하기위해 좌표 인식

--- ts/src/main/webapp/resources/scripts/maps/mainMap.js
+++ ts/src/main/webapp/resources/scripts/maps/mainMap.js
@@ -1,9 +1,38 @@
-// Default infoBox Rating Type
-var INFOBOX_RATINGTYPE = 'star-rating';
-var IMAGE_URL = 'http://localhost:8090/jj/resources/images';
+// main Script
+//------------------------------------------------
+
+// Marker
+const CURRENT_MAP_ICO = "<i class='im im-icon-Geek-2'></i>";
+const LECTURE_MAP_ICO = "<i class='im im-icon-Street-View'></i>";
+
+// Map
+const elementMap = document.getElementById('map');
+
+// Is MapScrollwheel abale Attributes
+const mapScrollAttr = document.querySelector('#map').dataset.mapScroll;
+
+// Scroll enabling button
+const scrollElement = document.querySelector('#scrollEnabling');
+
+// Geo Location Button 
+const currentGeoLocation = document.querySelector('#geoLocation');
+
+// Geo GPS 
+const GPS = document.querySelector('#gps');
+
+// Steet View Button
+const STREETELEMENT = document.querySelector("#streetView");
+
+// get start lecture 
+const GET_LECTURE = document.querySelector("#start");
+
+
+let LECTURE_LAT;
+let LECTURE_LNG;
+
 // Google Map Style
 // https://mapstyle.withgoogle.com/
-var MAINMAP_STYLE = [{
+const MAP_STYLE = [{
   "featureType": "poi",
   "elementType": "labels.text.fill",
   "stylers": [{ "color": "#747474" }, { "lightness": "23" }]
@@ -105,327 +134,83 @@ var MAINMAP_STYLE = [{
   "stylers": [{ "color": "#d2e7f7" }]
 }];
 
-// Custom User Interface Elements
-// Custom Zoom-In and Zoom-Out Buttons
-// ----------------------------------------------- //
-function ZoomControl(zoomControlDiv, map) {
-  zoomControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(zoomControlDiv);
-
-  // Creating divs & styles for custom zoom control
-  zoomControlDiv.style.padding = '5px';
-  zoomControlDiv.className = "zoomControlWrapper";
-
-  // Set CSS for the control wrapper
-  var controlWrapper = document.createElement('div');
-  zoomControlDiv.appendChild(controlWrapper);
-
-  // Set CSS for the zoomIn
-  var zoomInButton = document.createElement('div');
-  zoomInButton.className = "custom-zoom-in";
-  controlWrapper.appendChild(zoomInButton);
-
-  // Set CSS for the zoomOut
-  var zoomOutButton = document.createElement('div');
-  zoomOutButton.className = "custom-zoom-out";
-  controlWrapper.appendChild(zoomOutButton);
-
-  // Setup the click event listener - zoomIn
-  google.maps.event.addDomListener(zoomInButton, 'click', function () {
-    map.setZoom(map.getZoom() + 1);
-  });
-
-  // Setup the click event listener - zoomOut
-  google.maps.event.addDomListener(zoomOutButton, 'click', function () {
-    map.setZoom(map.getZoom() - 1);
-  });
+// default Google config
+const tools = {
+  position: {
+    lat: parseFloat(37.3232287),
+    lng: parseFloat(126.8384344),
+  },
+  MAP_STYLE,
+  elementMap,
+  mapScrollAttr,
+  scrollElement,
+  currentGeoLocation,
 }
 
-// Infobox Output
-function locationData(customMap) {
-  return (''+
-    `
-  <a href=${customMap.lectureURL} class="listing-img-container">
-    <div class="infoBox-close"><i class="fa fa-times"></i></div>
-    <img src="${customMap.lectureImg}" alt="${customMap.teacher_name}">
-    <div class="listing-item-content">
-      <h3>${customMap.lectureTitle}</h3>
-      <span>${customMap.teacher_name}</span>
-    </div>
-  </a>
+// object
+let googleMap;
 
-  <div class="listing-content">
-    <div class="listing-title">
-      <div class="${INFOBOX_RATINGTYPE}" data-rating="${customMap.lectureRating}">
-        <div class="rating-counter">(${customMap.lectureReviewCounter} reviews)</div>
-      </div>
-    </div>
-  </div>
-  `);
-}
+// Geo Location Current & Create select Button
+async function setNavigation() {
+  await googleMap.removePinMarker();
+  await googleMap.geolocate();
+  await googleMap.addPinMarker(CURRENT_MAP_ICO, 1, googleMap.position.lat, googleMap.position.lng);
+  await googleMap.addPinMarker(LECTURE_MAP_ICO, 2, LECTURE_LAT, LECTURE_LNG);
 
-// Geo Location Button
-function geolocate(map) {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (current) {
-      var position = new google.maps.LatLng(current.coords.latitude, current.coords.longitude);
-      map.setCenter(position);
-      map.setZoom(15);
-    });
+  const singleListingMapContainer = document.querySelector("#map-container");
+  const checkSelect = singleListingMapContainer.querySelector(".js_route_mode_way");
+  if (!checkSelect || typeof (checkSelect) == 'undefined' && checkSelect == null) {
+    const selector = document.createElement("select");
+    selector.classList.add("js_route_mode_way")
+    selector.innerHTML = `
+            <option value="DRIVING">Driving</option>
+            <option value="WALKING">Walking</option>
+            <option value="BICYCLING">Bicycling</option>
+            <option value="TRANSIT">Transit</option>
+        `;
+    singleListingMapContainer.appendChild(selector);
+  }
+  else {
+    // Route find listener  
+    await googleMap.routeFind(singleListingMapContainer, LECTURE_LAT, LECTURE_LNG);
   }
 }
 
- // Map Attributes
-var mapScrollAttr = document.querySelector('#map').dataset.mapScroll;
-
-// Scroll enabling button
-var scrollEnabling = document.querySelector('#scrollEnabling');
-
-// Geo Location Button 
-var currentGeoLocation = document.querySelector('#geoLocation');
-var customcurrentGeoLocation = document.querySelector('.input-with-icon.location a i')
-
-function mainMap(locations, position, mapZoom) {
-  // Map setting 
-  // Map Attributes
-  // ----------------------------------------------- //
-
-  var zoomLevel, scrollEnabled;
-  if (mapZoom && typeof mapZoom !== "undefined") {
-    zoomLevel = parseInt(mapZoom);
-  } else {
-    zoomLevel = 5;
-  }
-
-  if (mapScrollAttr && typeof mapScrollAttr !== "undefined") {
-    scrollEnabled = parseInt(mapScrollAttr);
-  } else {
-    scrollEnabled = false;
-  }
-
-  // Main Map
-  // ----------------------------------------------- //
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: zoomLevel,
-    scrollwheel: scrollEnabled,
-    // korea default
-    center: new google.maps.LatLng(position.lat, position.lng),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoomControl: false,
-    mapTypeControl: false,
-    scaleControl: false,
-    panControl: false,
-    navigationControl: false,
-    streetViewControl: false,
-    gestureHandling: 'cooperative',
-    styles: MAINMAP_STYLE
+function init() {
+  GPS.addEventListener("click", () => {
+    setNavigation();
   });
 
-  var zoomControlDiv = document.createElement('div');
-  var zoomControl = new ZoomControl(zoomControlDiv, map);
-
-  // Scroll enabling button addEventListener
-  // ----------------------------------------------- //
-  if (scrollEnabling) {
-    scrollEnabling.addEventListener('click', function (event) {
-      event.preventDefault();
-      if (this.matches(".enabled")) {
-        map.setOptions({ 'scrollwheel': false });
-        this.classList.remove("enabled");
-      } else {
-        map.setOptions({ 'scrollwheel': true });
-        this.classList.add("enabled");
-      }
-    },
-      {
-        capture: false,
-        passive: false
-      }
-    );
+  if (elementMap && typeof (elementMap) !== 'undefined') {
+    googleMap = new GoogleMap(tools);
+    googleMap.setStreetView(STREETELEMENT);
+    google.maps.event.addDomListener(window, 'load', googleMap.map);
   }
-  // Geo Location Button addEventListener
-  // ----------------------------------------------- //
-  if (currentGeoLocation) {
-    currentGeoLocation.addEventListener('click', function (event) {
-      event.preventDefault();
-      geolocate(map);
-    },
-      {
-        capture: false,
-        passive: false
-      }
-    );
-  }
-  // Geo Location Button <a> tag   
-  if (customcurrentGeoLocation) {
-    customcurrentGeoLocation.addEventListener('click', function (event) {
-      event.preventDefault();
-      geolocate(map);
-    }, {
-      capture: false,
-      passive: false
-    }
-    );
-  }
+  GET_LECTURE.addEventListener("click", () => {
+    googleMap.geolocate();
+    getBounds();
+    GET_LECTURE.style.visibility = "hidden";
+  });
+};
 
-  // Infobox(pin)
-  // http://htmlpreview.github.io/?https://github.com/googlemaps/v3-utility-library/blob/master/infobox/docs/reference.html
-  // ----------------------------------------------- //
-  var infobox = new InfoBox();
-
-  // Chosen Rating Type
-  google.maps.event.addListener(infobox, 'domready', function () {
-    starRating(`.infoBox .${INFOBOX_RATINGTYPE}`);
+async function getBounds() {
+  // 줌 또는 드래그, 화면이동 등 지도 정보 변경시에 화면내에 마커만 표시하기위해 좌표 인식
+  google.maps.event.addListener(googleMap.map, 'zoom_changed', () => {
+    boundsCheck();
   });
 
-  var pinMarkers = [];
-  var boxText = document.createElement("div");
-  boxText.className = 'map-box';
-  var boxOptions = {
-    content: boxText,
-    disableAutoPan: false,
-    alignBottom: true,
-    maxWidth: 0,
-    pixelOffset: new google.maps.Size(-134, -55),
-    zIndex: null,
-    boxStyle: {
-      width: "270px"
-    },
-    closeBoxMargin: "0",
-    closeBoxURL: "",
-    infoBoxClearance: new google.maps.Size(300, 100),
-    isHidden: false,
-    pane: "floatPane",
-    enableEventPropagation: false,
-  };
-  for (var locations_number = 1; locations_number < locations.length; locations_number++) {
-    // pin custom config adapt
-    var markerIcon = locations[locations_number]['pinImg'];
-    var overlaypositions = new google.maps.LatLng(locations[locations_number]['lat'], locations[locations_number]['lng']);
-    var overlayPinMarker = new CustomMarker(
-      overlaypositions,
-      map,
-      {
-        marker_id: locations[locations_number]['pinMarkerId'],
-      },
-      markerIcon
-    );
-    pinMarkers.push(overlayPinMarker);
-    // infobox custom config adat
-    google.maps.event.addDomListener(overlayPinMarker, 'click', (function (overlayPinMarker, locations_number) {
-      return function () {
-        boxText.innerHTML = locations[locations_number]['html'];
-        infobox.setOptions(boxOptions);
-        infobox.close();
-        infobox.open(map, overlayPinMarker);
-
-        google.maps.event.addListener(infobox, 'domready', function () {
-          document.querySelector('.infoBox-close').addEventListener('click', function (event) {
-            event.preventDefault();
-            infobox.close();
-            document.querySelectorAll('.map-marker-container').forEach(function(tag){
-              tag.classList.remove('clicked', 'infoBox-opened');
-            });
-          });
-        },
-          {
-            capture: false,
-            passive: false
-          }
-        );
-      }
-    })(overlayPinMarker, locations_number));
-  }
-
-  // Marker highlighting when hovering listing item
-  document.querySelectorAll('.listing-item-container').forEach(function (tag) {
-    tag.addEventListener('mouseover', function () {
-      var markerId = tag.dataset.markerId;
-      if (markerId && typeof markerId !== "undefined") {
-        var markerId = parseInt(markerId) - 1;
-        var marker_div = pinMarkers[markerId].div;
-        marker_div.classList.add('clicked');
-
-        tag.addEventListener('mouseout', function () {
-          if (marker_div.matches(":not(.infoBox-opened)")) {
-            marker_div.classList.remove('clicked');
-          }
-        });
-      }
-    });
-  });
-
-  // Marker Clusterer Init
-  // ----------------------------------------------- //
-  var clusterStyles = [
-    {
-      textColor: 'white',
-      url: '',
-      height: 50,
-      width: 50
-    }
-  ];
-
-  var options = {
-    imagePath: 'images/',
-    styles: clusterStyles,
-    minClusterSize: 2
-  };
-  var markerCluster = new MarkerClusterer(map, pinMarkers, options);
-
-  google.maps.event.addDomListener(window, "resize", function () {
-    var center = map.getCenter();
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center);
-    Promise.resolve().then(someMethodIThinkMightBeSlow);
+  google.maps.event.addListener(googleMap.map, 'dragend', () => {
+    boundsCheck();
   });
 }
-// ---------------- Main Map / End ---------------- //
 
-var startTime;
-function initMap(){
-  startTime = performance.now();
-  var mapZoom = 8;
-  var position ={
-    lat: 37.3232287,
-    lng: 126.8384344,
-  };
-  var locations = [
-    {},
-    {
-      html: locationData('listings-single-page.html', `${IMAGE_URL}/classImages/listing-item-01.png`, "Tom's Restaurant", '964 School Street, New York', '3.5', '12'),
-      lat: 40.94401669296697,
-      lng: -74.16938781738281,
-      pinMarkerId: 1,
-      pinImg: '<i class="im im-icon-Chef-Hat"></i>'
-    },
-  ];
-
-  var map = document.getElementById('map');
-  if (map && typeof (map) !== 'undefined') {
-    google.maps.event.addDomListener(window, 'load', mainMap(locations,position,mapZoom));
-  }
+function boundsCheck() {
+  // bounds lat, lng 
+  const startLat = googleMap.map.getBounds().getSouthWest().lat();
+  const startLng = googleMap.map.getBounds().getSouthWest().lng();
+  const endLat = googleMap.map.getBounds().getNorthEast().lat();
+  const endLng = googleMap.map.getBounds().getNorthEast().lng();
+  getLecture(startLat, startLng, endLat, endLng);
 }
 
-function searchMap(customMap) {
-  startTime = performance.now();
-  var mapZoom = customMap.mapZoom;
-  var position = {
-    lat: customMap.lat,
-    lng: customMap.lng,
-  }
-  var locations = [
-    {},
-    {
-      html: locationData(customMap),
-      lat: customMap.lat,
-      lng: customMap.lng,
-      pinMarkerId: 0,
-      pinImg: `<i class="im ${customMap.pinImg}"></i>`
-    },
-  ];
-  google.maps.event.addDomListener(window, 'load', mainMap(locations, position, mapZoom));
-}
-
-Promise.resolve(initMap()).then(someMethodIThinkMightBeSlow(startTime));
-
+init();
